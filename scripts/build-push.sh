4becafd3f81bcc2a3c48864581a88e59ab096546
Benny <benny@yjx.com>
2021-06-04 11:29:43 +0800

    modified build-push.sh

--- scripts/build-push.sh
+++ scripts/build-push.sh
@@ -1,14 +1,17 @@
 #!/usr/bin/env bash
 
-set -e
-set -x
+#set -e
+#set -x
+
+read -p "dockerhub用户名："  DOCKER_USERNAME
+read -s -p "dockerhub密码："  DOCKER_PASSWORD
 
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 
-docker build -t tiangolo/node-frontend:10 .
+docker build -t suimong/node-frontend:12 ..
 
-docker build -t tiangolo/node-frontend:latest .
+docker build -t suimong/node-frontend:latest ..
 
-docker push tiangolo/node-frontend:10
+docker push suimong/node-frontend:12
 
-docker push tiangolo/node-frontend:latest
+docker push suimong/node-frontend:latest
